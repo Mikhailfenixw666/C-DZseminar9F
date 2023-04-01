4b3e00e7ab7097ef3557169f9ef86a28617f73c8
Mikhailfenixw666 <124437837+Mikhailfenixw666@users.noreply.github.com>
2023-04-01 11:15:49 +0300

    HomeWork task1(68)

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,10 +4,8 @@
 
 Console.WriteLine("Введите начальное число M:");
 int numberM = int.Parse(Console.ReadLine());
-
 Console.WriteLine("Введите начальное число N:");
 int numberN = int.Parse(Console.ReadLine());
-
 int AckermannFunction(int numberM, int numberN)
 {
   if (numberM == 0) return numberN + 1;
